3ef11d03a36799b928376b606e03a87372bffd3a
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-15 15:46:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,15 +43,7 @@ def over?(board)
 end 
 
 def winner(board)
-  index = []
-  index = won?(board)
-  if index == false
-    return nil
-  else
-    if board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
+  if won?(board)
+    
+
 end 
